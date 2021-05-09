6f70384c705ac74537f8dadbc78a6a37a517258f
Tom Fraczek <tomfraczekdev@gmail.com>
2021-05-09 21:19:48 +0200

    responsivnes added

--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import mainImage from './assets/homepage';
+import logo from '../../components/header/assets/logo.png';
 
 import './homepage.styles.scss';
 
@@ -8,8 +9,7 @@ const Homepage = () => (
     <div className="homepage">
     
     <div className="banner-text">
-        <h3>Welcome to the World of Movies</h3>
-        <h4>Movie Infromation House</h4>
+        <img className='banner-logo' src={logo} alt=""/>
     </div>
     <img src={mainImage} alt="homepage image"/>
     </div>
